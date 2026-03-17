ca510880ec44a24fbd6628cf1a278762708a502a
Joe Weir <2770814W@student.gla.ac.uk>
2026-03-17 20:47:32 +0000

    Developed more scripts to make graphs and moved all data into data folder.

---
+++ data/Clean_per_response_data.py
@@ -0,0 +1,23 @@
+import csv
+
+
+def main():
+    input_file = "raw/PerVisualisationRaw.csv"
+    output_file = "processed/PerVisualisation_cleaned.csv"
+
+    with open(input_file, newline='', encoding='utf-8') as infile, \
+         open(output_file, "w", newline='', encoding='utf-8') as outfile:
+
+        reader = csv.DictReader(infile)
+        fieldnames = reader.fieldnames[17:]
+        writer = csv.DictWriter(outfile, fieldnames=fieldnames)
+
+        writer.writeheader()
+
+        for row in reader:
+            filtered_row = {key: row[key] for key in fieldnames}
+            writer.writerow(filtered_row)
+
+
+if __name__ == "__main__":
+    main()
